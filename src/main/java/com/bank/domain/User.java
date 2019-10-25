6215e9995c3e0d792a4270edfd83ac40ddfdc8b8
Gabe <popovici.gabriel@gmail.com>
2019-10-26 01:30:22 +0300

    add moneta and user

---
+++ src/main/java/com/bank/domain/User.java
@@ -0,0 +1,19 @@
+package com.bank.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private long userId;
+
+    private String userName;
+
+    private String emailAddress;
+}
