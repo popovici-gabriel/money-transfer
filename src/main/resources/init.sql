957068d4c7a3b4ed21eeb2c60c2631525b4c680e
Gabe <popovici.gabriel@gmail.com>
2019-10-26 01:06:34 +0300

    add DDL script

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,8 +1,18 @@
-DROP TABLE IF EXISTS billionaires;
+DROP TABLE IF EXISTS User;
 
-CREATE TABLE billionaires (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
-);
+CREATE TABLE User (
+UserId LONG PRIMARY KEY AUTO_INCREMENT NOT NULL,
+UserName VARCHAR(30) NOT NULL,
+EmailAddress VARCHAR(30) NOT NULL);
+
+CREATE UNIQUE INDEX idx_ue on User(UserName,EmailAddress);
+
+DROP TABLE IF EXISTS Account;
+
+CREATE TABLE Account (
+AccountId LONG PRIMARY KEY AUTO_INCREMENT NOT NULL,
+UserName VARCHAR(30),
+Balance DECIMAL(19,4),
+CurrencyCode VARCHAR(30));
+
+CREATE UNIQUE INDEX idx_acc on Account(UserName,CurrencyCode);
