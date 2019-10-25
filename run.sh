e166600d76273a1b4d24d2423312424a423b621e
Gabe <popovici.gabriel@gmail.com>
2019-10-25 20:25:24 +0300

    adding logback and sl4j run.sh script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo '--> About to run Revolut Bank Money Transfer App...'
+java -jar target/money-transfer-1.0.0-SNAPSHOT.jar
+
+echo '------------------------------------------'
+echo '------------------------------------------'
+
+echo '------------------------------------------'
+echo '----------------THE END-------------------'
--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -1,8 +1,14 @@
 package com.bank;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BankApplication {
 
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(BankApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Yellow");
+        LOGGER.info("Starting most advanced Bank application...");
     }
 }
