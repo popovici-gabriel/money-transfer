2675e4ec8def105cf9f95a8711fb75c0f505aa3f
Gabe <popovici.gabriel@gmail.com>
2019-11-15 22:12:49 +0200

    make the transfer async

--- src/main/java/com/bank/resource/TransferResource.java
+++ src/main/java/com/bank/resource/TransferResource.java
@@ -1,6 +1,7 @@
 package com.bank.resource;
 
 
+import com.bank.domain.Account;
 import com.bank.domain.AccountNotFound;
 import com.bank.domain.InsufficientFundsException;
 import com.bank.domain.TransferService;
@@ -16,6 +17,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import static com.bank.domain.Account.USD;
 import static java.lang.String.format;
@@ -50,24 +53,68 @@ public class TransferResource {
                 || toAccountId == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
-        int errorStatus;
+        Account fromAccount;
+        Account toAccount;
         try {
-            final var fromAccount = accountRepository.findByAccountId(fromAccountId);
-            final var toAccount = accountRepository.findByAccountId(toAccountId);
-            final var moneyAmount = Money.of(new BigDecimal(amount), USD);
-            return Response
-                    .ok(transferService.transferMoney(fromAccount, toAccount, moneyAmount, 1, SECONDS))
-                    .build();
+            fromAccount = accountRepository.findByAccountId(fromAccountId);
+            toAccount = accountRepository.findByAccountId(toAccountId);
         } catch (AccountNotFound e) {
             LOGGER.error("Account not found", e);
-            errorStatus = HttpStatus.NOT_FOUND_404;
-        } catch (InsufficientFundsException e) {
-            LOGGER.error(format("Insufficient funds from %s", fromAccountId), e);
-            errorStatus = HttpStatus.NOT_MODIFIED_304;
-        } catch (InterruptedException e) {
-            LOGGER.error("Transfer error", e);
-            errorStatus = HttpStatus.INTERNAL_SERVER_ERROR_500;
+            return Response.status(HttpStatus.NOT_FOUND_404).build();
         }
-        return Response.status(errorStatus).build();
+
+        final var moneyAmount = Money.of(new BigDecimal(amount), USD);
+        TransferStatus transferStatus;
+        try {
+            transferStatus = transferAsync(fromAccount, toAccount, moneyAmount, 1, SECONDS);
+        } catch (Exception e) {
+            LOGGER.error("Internal error", e);
+            return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+        }
+
+        if (transferStatus.error) {
+            return Response.status(transferStatus.errorStatus).build();
+        }
+        return Response.ok(transferStatus.result).build();
+    }
+
+    private TransferStatus transferAsync(Account fromAccount, Account toAccount, Money moneyAmount, long timeout, TimeUnit timeUnit) {
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    boolean error = false;
+                    int errorStatus = 0;
+                    String errorReason = null;
+                    boolean result = false;
+                    try {
+                        result = transferService.transferMoney(fromAccount, toAccount, moneyAmount, timeout, timeUnit);
+                    } catch (InsufficientFundsException e) {
+                        LOGGER.error(format("Insufficient funds from %s", fromAccount.getAccountId()), e);
+                        error = true;
+                        errorStatus = HttpStatus.NOT_MODIFIED_304;
+                        errorReason = String.format("Insufficient funds from %s balance = %s ", fromAccount.getAccountId(), fromAccount.getBalance());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        LOGGER.error("Transfer error", e);
+                        error = true;
+                        errorStatus = HttpStatus.INTERNAL_SERVER_ERROR_500;
+                        errorReason = "Internal error when performing transfer";
+                    }
+                    return new TransferStatus(error, result, errorStatus, errorReason);
+                })
+                .join();
+    }
+
+    private static final class TransferStatus {
+        private boolean error;
+        private boolean result;
+        private int errorStatus;
+        private String errorReason;
+
+        public TransferStatus(boolean error, boolean result, int errorStatus, String errorReason) {
+            this.error = error;
+            this.result = result;
+            this.errorStatus = errorStatus;
+            this.errorReason = errorReason;
+        }
     }
 }
